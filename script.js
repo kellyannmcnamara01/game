22928887e4e81c2f8ec96d4d8a524a911ed9b563
jsawicz <jessicasawicz@gmail.com>
2017-05-12 16:22:32 -0400

    added input/output

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert("hi");
+//alert("hi");
+
+var userinput = document.getElementById("userinput");
+var output = document.getElementById("output");
+var submitbtn = document.getElementById("submit");
+
+submitbtn.onclick = function() {
+	var output.innerHTML = userinput;
+	return false;
+}
