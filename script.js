bf1f755f1c11f219004dcfd3ba64b1ffa5f5dca4
kellyannmcnamara01 <kellyannmcnamara01@gmail.com>
2017-05-12 16:56:22 -0400

    added regex

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-//alert("hi");
+window.onload = function() {
+    //alert("hi");
 
-var userinput = document.getElementById("userinput");
-var output = document.getElementById("output");
-var submitbtn = document.getElementById("submit");
+    //vars
+    var userinput = document.getElementById("userinput");
+    var output = document.getElementById("output");
+    var submitbtn = document.getElementById("submit");
+    var userinputVal = /^([A-Za-z]{1})$/;
 
-submitbtn.onclick = function() {
-	output.innerHTML = userinput;
-	return false;
-}
+    //check user input to regex
+    function checkInput() {
+        var userinputReg = /^([A-Za-z]{1})$/;
+        if(userinputReg.test(userinput.value)) {
+            output.innerHTML = userinput.value;
+            return false;
+        } else {
+            output.innerHTML = "boo";
+            return false;
+        }
+    } //end checkInput
+
+    //call checkinput on submitbtn click
+    submitbtn.onclick = checkInput;
+
+}; //end onload
