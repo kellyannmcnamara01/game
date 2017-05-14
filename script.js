5540751bdfea8229e20d13a40acdfb389f1a0d3e
jsawicz <jessicasawicz@gmail.com>
2017-05-14 02:31:35 -0400

    added array of words and randomizer logic

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ window.onload = function() {
     //4. if user input is correct display entered number if user input is wrong display wrong letters in a different div
     //5. add correct user letters to correct letters array and display array
     //6. add wrong user letters to wrong letters array and display
+	//7. Grab individual letters from words
+	//8. Randomize possible words
+	//9. Check guess within the answer
 
 
     //vars
@@ -13,24 +16,36 @@ window.onload = function() {
     var submitbtn = document.getElementById("submit");
 	var wrongdiv = document.getElementById("wrong");
 	var rightdiv = document.getElementById("correct");
+	var gspaces = document.getElementById("gameSpaces");
 	
 	//array of guessed letters
 	var rightLetters = [];
 	var wrongLetters = [];
+	var letter = "G";
 	
 	//array of word arrays
 	var words = [
 		["b", "o", "o", "y", "a"],
 		["b", "a", "m"],
-		["c", "a", "n", "d", "y"]
+		["c", "a", "n", "d", "y"],
+		["s", "u", "p", "e", "r", "c", "a", "l"],
+		["t", "h", "i", "s"]
 		];
 	
 	//randomize word selection
 	var answer = words[Math.floor(Math.random() * words.length)];
 	
-	//put _ for each letter in the random word
+	//alert(answer);
 	
-
+	//put randomized word into a new array to replace with _ in the next step
+	var spaces = new Array(answer.length);
+	
+	//put _ for each letter in the random word
+	for (var i = 0; i < spaces.length; i++){
+	spaces[i] = "_ ";
+	gspaces.innerHTML = spaces;
+	}
+	
     //check user input to regex
     function checkInput() {
         var userinputReg = /^([A-Za-z]{1})$/;
